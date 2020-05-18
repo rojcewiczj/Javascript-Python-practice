091e4b8daecc917ca4d55c6726b64f87f9f8527a
John Rojcewicz <rojcewiczj@gmail.com>
2020-05-18 16:47:21 -0500

    simplifyed method

--- Python-Practice/final_minimum_bribe.py
+++ Python-Practice/final_minimum_bribe.py
@@ -1,31 +1,25 @@
 def minimumBribes(q):
     too_chaotic = False
-    sec_dicti = {}
     count = 0
     inc = len(q) - 2
-
-    for index, num in enumerate(q):
-        sec_dicti[index] = num
-
     while inc > - 1:
-        
-        if sec_dicti[inc] > sec_dicti[inc + 1]:
-            first_place = sec_dicti[inc]
-            second_place = sec_dicti[inc + 1]
+        if  q[inc] > q[inc + 1]:
+            first_place = q[inc]
+            second_place = q[inc + 1]
             print(first_place, second_place)
-            sec_dicti[inc + 1] = first_place
-            sec_dicti[inc] = second_place
-            print(sec_dicti[inc], sec_dicti[inc + 1])
+            q[inc + 1] = first_place
+            q[inc] = second_place
+            print(q[inc], q[inc + 1])
             count += 1
             if inc < len(q) - 2:
-                if first_place > sec_dicti[inc + 2]:
-                    third_place = sec_dicti[inc + 2]
-                    sec_dicti[inc + 1] = third_place
-                    sec_dicti[inc + 2] = first_place
+                if first_place > q[inc + 2]:
+                    third_place = q[inc + 2]
+                    q[inc + 1] = third_place
+                    q[inc + 2] = first_place
                     count += 1
-                    print(sec_dicti[inc], sec_dicti[inc + 1], sec_dicti[inc + 2])
+                    print(q[inc], q[inc + 1], q[inc + 2])
                 if inc < len(q) - 3:
-                    if first_place > sec_dicti[inc + 3]:
+                    if first_place > q[inc + 3]:
                         too_chaotic = True
 
         inc -= 1
