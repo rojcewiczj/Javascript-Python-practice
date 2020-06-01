b3a269518ff348aaac0cad7f21371b4eae2de870
John Rojcewicz <rojcewiczj@gmail.com>
2020-06-01 15:32:52 -0500

    completed sorting scores algorithm

---
+++ Python-Practice/sorting_scores.py
@@ -0,0 +1,19 @@
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def __repr__(self):
+        pass
+       
+    def comparator(a, b):
+        if a.score > b.score:
+            return - 1
+        elif a.score < b.score:
+            return 1
+        elif a.score == b.score:
+            if a.name > b.name:
+                return 1
+            elif a.name < b.name:
+                return -1
+            else:
+                return 0
