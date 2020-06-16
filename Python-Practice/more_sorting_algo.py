1c757e9161074d0317c5b20ae25864f620bcdcc1
John Rojcewicz <rojcewiczj@gmail.com>
2020-06-16 14:20:16 -0500

    more algo practice

---
+++ Python-Practice/more_sorting_algo.py
@@ -0,0 +1,30 @@
+def activityNotifications(expenditure, d):
+    inc = 0
+    sec_inc = 0
+    third_inc = 0
+    exp = []
+    count = d
+    notification = 0
+    while  inc  < len(expenditure) :
+        if inc < count:
+            bisect.insort(exp, expenditure[inc]) 
+            inc += 1
+        elif inc == count:
+            n = len(exp)
+            
+            if n % 2 == 0:
+                median1 = exp[n// 2]
+                median2 = exp[n// 2 -1]
+                median = (median1 + median2) / 2
+            else:
+                median = exp[n // 2]
+            
+            if expenditure[count] / median >= 2:
+                notification += 1
+
+            exp = []
+            sec_inc += 1
+            inc = sec_inc
+            count += 1
+        
+    return notification
