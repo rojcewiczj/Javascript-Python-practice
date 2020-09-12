1dd83da1f7963f64be6dc2d23d10449209e9f7af
John Rojcewicz <rojcewiczj@gmail.com>
2020-09-11 22:46:29 -0500

    more practice

--- Python-Practice/multi-player_cards.py
+++ Python-Practice/multi-player_cards.py
@@ -1,30 +1,32 @@
 
 
 def player_cards(num_of_players, deck):
-    dicti = {}
-    i = num_of_players
-    for i in range(1, num_of_players + 1):
-        dicti[i] = []
-    print(dicti)
-    i = 1
-    j = len(deck) - 1
-    while j > -1:
-        print(i)
-        dicti[i].append(deck[j])
-        if i == num_of_players:
-            i = 1
-        else:
-            i += 1
-        
-        j -= 1
-
-    print(dicti)
     rounds = [0] * (len(deck) // num_of_players)
-    for i in range(0,len(rounds)):
-        for key in dicti:
-            if dicti[key][i + 1] > rounds[i]:
-                
-    print(rounds)
+    nums = [0] * num_of_players
+    check = len(deck) - num_of_players - 1
+    
+    i = len(rounds)
+    l = 0
+    k = 0
+    while i > 0:
+        max = 0
+        for j in reversed(range(check + 1, check + num_of_players + 1)):
+            nums[k] = deck[j]
+            if nums[k] > max:
+                highest = k + 1
+                max = nums[k]
+           
+            k += 1
+        
+        rounds[l] = highest
+        l += 1
+        k = 0
+        max = 0
+        check = check - num_of_players 
+        print(nums, highest, rounds, l)
+        nums = [0] * num_of_players
+        i -= 1
+    
 
 
 player_cards(3,[1,2,5,3,1,7,8,4,6,3,9,5])
