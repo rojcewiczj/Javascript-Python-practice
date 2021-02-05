2657088a2906ee2229087f1e68fecc8dd5ca7ae3
John Rojcewicz <rojcewiczj@gmail.com>
2021-02-04 23:31:43 -0600

    more algoExpert practice with JavaScript

---
+++ Javascript-Practice/MInimumWaitingTime.js
@@ -0,0 +1,8 @@
+function minimumWaitingTime(queries) {
+    // Write your code here.
+    return 0;
+  }
+  
+  // Do not edit the line below.
+  exports.minimumWaitingTime = minimumWaitingTime;
+  
