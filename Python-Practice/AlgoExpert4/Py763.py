7b18758f0930babb5aae05d68ee3040999f5d15c
John Rojcewicz <rojcewiczj@gmail.com>
2021-07-06 23:03:37 -0500

    python practice

---
+++ Python-Practice/AlgoExpert4/Py763.py
@@ -0,0 +1,12 @@
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self, head) -> None:
+        self.head = head
+
+
+
