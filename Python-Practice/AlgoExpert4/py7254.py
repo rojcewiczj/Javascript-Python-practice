be926992da04001a561bf663fe29c96d20f9a16f
John Rojcewicz <rojcewiczj@gmail.com>
2021-07-25 21:45:18 -0500

    python practice

---
+++ Python-Practice/AlgoExpert4/py7254.py
@@ -0,0 +1,20 @@
+class Time_loop:
+    def __init__(self):
+        self.time = []
+
+    def add(self, event):
+        if len(self.time) < 10:
+            self.time.append(event)
+        else:
+            pass
+         
+
+
+
+def round():
+    sacred_time_line = []
+
+
+
+
+print(round())
